41b2e756d1b87db60f307793f2953c395f70e113
Dominik Bathon <dominik.bathon@gmail.com>
2020-10-06 00:49:28 +0200

    Implement IsTypeOperator

--- src/main/java/de/dbathon/jds/service/FilterOperator.java
+++ src/main/java/de/dbathon/jds/service/FilterOperator.java
@@ -3,9 +3,12 @@ package de.dbathon.jds.service;
 import static de.dbathon.jds.util.JsonUtil.toJsonString;
 import static java.util.Objects.requireNonNull;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,7 +29,9 @@ public abstract class FilterOperator {
     filterOperators.put(">", new SimpleOperator(">", false, String.class, JsonStringNumber.class));
     filterOperators.put(">=", new SimpleOperator(">=", false, String.class, JsonStringNumber.class));
 
-    // TODO: is (string, number, null, undefined etc.), prefix (mainly for id), like, ilike, in
+    filterOperators.put("is", new IsTypeOperator());
+
+    // TODO: prefix (mainly for id), like, ilike, in
 
     FILTER_OPERATORS = Collections.unmodifiableMap(filterOperators);
   }
@@ -47,6 +52,25 @@ public abstract class FilterOperator {
 
   protected static boolean isSpecialKey(final String key) {
     return DocumentService.ID_PROPERTY.equals(key) || DocumentService.VERSION_PROPERTY.equals(key);
+  }
+
+  protected String getJsonPathExpression(final String key) {
+    if (!VALID_KEY_PATTERN.matcher(key).matches()) {
+      throw new ApiException("invalid filter key: " + key);
+    }
+    final StringBuilder result = new StringBuilder("data");
+    final Matcher matcher = NAME_OR_INDEX_PATTERN.matcher(key);
+    while (matcher.find()) {
+      final String match = matcher.group();
+      if (match.startsWith("[")) {
+        result.append("->").append(match.substring(1, match.length() - 1));
+      }
+      else {
+        // no need for escaping, the pattern allows only save parameters
+        result.append("->'").append(match).append("'");
+      }
+    }
+    return result.toString();
   }
 
   private static class SimpleOperator extends FilterOperator {
@@ -78,25 +102,6 @@ public abstract class FilterOperator {
        * but we would like it to be "true", so always coalesce it to false.
        */
       queryBuilder.add((negate ? "not coalesce(" : "coalesce(") + expression + ", false)", parameters);
-    }
-
-    protected String getJsonPathExpression(final String key) {
-      if (!VALID_KEY_PATTERN.matcher(key).matches()) {
-        throw new ApiException("invalid filter key: " + key);
-      }
-      final StringBuilder result = new StringBuilder("data");
-      final Matcher matcher = NAME_OR_INDEX_PATTERN.matcher(key);
-      while (matcher.find()) {
-        final String match = matcher.group();
-        if (match.startsWith("[")) {
-          result.append("->").append(match.substring(1, match.length() - 1));
-        }
-        else {
-          // no need for escaping, the pattern allows only save parameters
-          result.append("->'").append(match).append("'");
-        }
-      }
-      return result.toString();
     }
 
     protected void applyNonSpecialKey(final QueryBuilder queryBuilder, final String key, final Object rightHandSide) {
@@ -207,6 +212,22 @@ public abstract class FilterOperator {
         // we can use the jsonb_path_ops index to optimize this
         addToQueryBuilder(queryBuilder, "data @> ?::jsonb", toJsonString(buildContainsValue(key, rightHandSide)));
       }
+    }
+
+  }
+
+  private static class IsTypeOperator extends FilterOperator {
+
+    private static final Set<String> EXPECTED_TYPES =
+        new HashSet<>(Arrays.asList("object", "array", "string", "number", "boolean", "null", "undefined"));
+
+    @Override
+    public void apply(final QueryBuilder queryBuilder, final String key, final Object rightHandSide) {
+      if (!EXPECTED_TYPES.contains(rightHandSide)) {
+        throw new ApiException("unexpected type for \"is\" operator: " + toJsonString(rightHandSide));
+      }
+
+      queryBuilder.add("coalesce(jsonb_typeof(" + getJsonPathExpression(key) + "), 'undefined') = ?", rightHandSide);
     }
 
   }
